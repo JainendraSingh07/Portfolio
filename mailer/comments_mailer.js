94e9f6bf4d7e10b5072b7ae33da5dd7bd275326d
JainendraSingh07 <jainendrasingh516@gmail.com>
2024-06-28 19:30:11 +0530

    setup smtp

--- mailer/comments_mailer.js
+++ mailer/comments_mailer.js
@@ -2,14 +2,14 @@ const nodeMailer = require('../config/nodemailer');
 
 // this is another way of exporting a method
 exports.newComment = async (comment) => {
-    console.log('inside newComment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment : comment}, '/comments/new_comment.ejs');
 
     try {
         let info = await nodeMailer.transporter.sendMail({
             from: 'WebAL.in',
             to: comment.user.email,
             subject: "New Comment Published!",
-            html: '<h1>Yup, your comment is now published!</h1>'
+            html: htmlString
         });
 
         console.log('Message sent', info);
